0a8535633df2ddf9670a9e46a1dc77f0825967f7
Doug Frye <noladoug@aol.com>
2017-03-12 18:06:07 -0500

    DoIt - 2nd page and back button in place

---
+++ DoIt/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Doug Frye on 3/12/17.
+//  Copyright © 2017 Doug Frye. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
